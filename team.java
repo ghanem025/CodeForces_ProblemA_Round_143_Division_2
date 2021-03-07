fd3c6b9634ac98523d896cb6e835b0e8d2612067
Ghanem <40302667+ghanem025@users.noreply.github.com>
2021-03-06 22:12:00 -0500

    Problem A Solution

---
+++ team.java
@@ -0,0 +1,30 @@
+import java.util.*;
+ 
+public class team{
+ 
+  public static void main(String [] args){
+    Scanner sc = new Scanner (System.in);
+    int partic = sc.nextInt();
+    int [] arr = new int[3];
+    int c=0;
+    int sol =0;
+    
+   for(int j=0;j<partic;j++){
+    c =0;
+    for(int i=0; i<3;i++){
+      int pvt = sc.nextInt();
+      arr[i] = pvt;
+      if(arr[i]>=1){
+        c++;
+      }
+    }
+    if(c>=2) {
+     sol++;
+    }
+    }
+    System.out.println(sol);
+
+ }
+}
+ 
+ 
